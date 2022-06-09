2c00776699dbb048ce9c325d164c383f9b30cdd3
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2022-06-09 01:25:36 -0400

    Things are coming along great! need to create unit tests and publish it on heroku and it'll be done :).

--- jsninjify/static/js/index.js
+++ jsninjify/static/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $("#ninjifybtn").click(function() {
+    $(".ninjifybtn").click(function() {
 
         let buzzwords = ($("#buzzwords").val()).toLowerCase().replaceAll(" ", ",")
 
@@ -10,11 +10,28 @@ $(document).ready(function(){
             data: { x: buzzwords },
             dataType: 'json',
             success: function(data) {
-                console.log(data.ninjaname)
+                if ($("#ninjanamebox").hasClass("hidden"))
+                    toggleNinjifyBox();
+                $("#ninjaname").text(data.ninjaname);
             },
             error: function(data){
                 console.log(data.responseText);
             }
         });
     });
+
+    $("#resetbtn").click(function(){
+        toggleNinjifyBox();
+        $("#ninjaname").text("");
+        $("#buzzwords").val("");
+    });
+
+    function toggleNinjifyBox(){
+        $(".ninjify-box").each(function(i, box){ 
+            let $box = $(box);
+            $box.hasClass("hidden") ? $box.removeClass("hidden") : $box.addClass("hidden");
+        });
+    }
+
 });
+
