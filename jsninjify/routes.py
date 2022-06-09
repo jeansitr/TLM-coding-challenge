95424d02052254ba349aa7ce903d4f66a0fd8514
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2022-06-08 23:38:56 -0400

    endpoint working, I gotta do some testing and actually showing the name on screen

--- jsninjify/routes.py
+++ jsninjify/routes.py
@@ -1,5 +1,55 @@
-from flask import current_app as app, render_template
+import random
+
+from jsninjify.models import Buzzword, Descriptive, Word
+from flask import Response, current_app as app, render_template, request, json
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/ninjify', methods=['GET'])
+def ninji_get():
+    buzzwords = request.args.get('x')
+    
+    if (buzzwords):
+        if not (";" and "--") in buzzwords:
+            #select words assiated to buzwords
+            descriptive = list(Buzzword
+                    .select(Buzzword.buzzword, Word.word)
+                    .join(Descriptive)
+                    .join(Word)
+                    .where(Buzzword.buzzword << buzzwords.split(","))
+                    .dicts())
+
+            #if none found
+            if len(descriptive) == 0:
+                response = Response(status=400, content_type="text/plain;charset=UTF-8", response="Buzzwords not found.")
+            else:
+                ninjaName = ""
+                
+                #select up to 4 words out of the ones that were found
+                for i in range(random.randrange(1, 4)):
+                    chosen = descriptive[random.randrange(1,len(descriptive))]["word"]
+
+                    if chosen not in ninjaName:
+                        ninjaName += chosen + " "
+
+                response = Response(status=200, content_type="application/json", response=json.dumps({ "ninjaname" : ninjaName}))
+        else:
+            response = Response(status=403, content_type="text/plain;charset=UTF-8", response="No injections here.")
+    else:
+        response = Response(status=400, content_type="text/plain;charset=UTF-8", response="Missing Buzzwords.")
+
+    return response
+
+@app.route('/ninjify', methods=['POST'])
+def ninji_post():
+    return Response(status=501, content_type="text/plain;charset=UTF-8", response="Can't add yet.")
+
+@app.route('/ninjify', methods=['PUT'])
+def ninji_put():
+    return Response(status=501, content_type="text/plain;charset=UTF-8", response="Can't update yet.")
+
+@app.route('/ninjify', methods=['DELETE'])
+def ninji_del():
+    return Response(status=501, content_type="text/plain;charset=UTF-8", response="Can't remove yet.")
---
+++ jsninjify/static/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $("#ninjifybtn").click(function() {
+
+        let buzzwords = ($("#buzzwords").val()).toLowerCase()
+
+        $.ajax({
+            url: "/ninjify",
+            type: 'GET',
+            data: { x: buzzwords },
+            dataType: 'json',
+            success: function(data) {
+                console.log(data.ninjaname)
+            },
+            error: function(data){
+                console.log(data.responseText);
+            }
+        });
+    });
+});
